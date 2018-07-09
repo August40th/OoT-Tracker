059894b6acdaf2306ec0e409219deeb88147d790
August40th <40985705+August40th@users.noreply.github.com>
2018-07-09 17:41:26 +0000

    Update main.js

--- script/main.js
+++ script/main.js
@@ -459,7 +459,7 @@ function initGridRow(itemsets) {
             itemGrid[r]['row'].style.display = 'none';
             itemGrid[r]['half'].style.display = 'none';
         } else {
-            for (c = 0; c < 10; c++) {
+            for (c = 0; c < 8; c++) {
                 if (!!itemsets[r][c] && itemsets[r][c] != 'blank') {
                     startdraw = true;
                     r++;
@@ -485,7 +485,7 @@ function initGridRow(itemsets) {
         }
         itemGrid[r]['button'].style.display = 'none';
 
-        for (c = 0; c < 7; c++) {
+        for (c = 0; c < 9; c++) {
             if (itemsets[r][c]) {
                 setGridItem(itemsets[r][c], r, c);
             } 
