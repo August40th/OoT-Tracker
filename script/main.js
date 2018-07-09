5bbaca647f341d579bce7a1733744411b643a5d4
August40th <40985705+August40th@users.noreply.github.com>
2018-07-09 07:54:54 +0000

    Update main.js

--- script/main.js
+++ script/main.js
@@ -485,7 +485,7 @@ function initGridRow(itemsets) {
         }
         itemGrid[r]['button'].style.display = 'none';
 
-        for (c = 0; c < 9; c++) {
+        for (c = 0; c < 7; c++) {
             if (itemsets[r][c]) {
                 setGridItem(itemsets[r][c], r, c);
             } 
