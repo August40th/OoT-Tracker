14b3383622652c3278834d926e3d66c759650ccb
August40th <40985705+August40th@users.noreply.github.com>
2018-07-13 10:10:41 +0000

    Update main.js

--- script/main.js
+++ script/main.js
@@ -425,7 +425,7 @@ function updateGridItem(row, index) {
 
 function updateGridItemAll() {
     for (r = 0; r < 8; r++) {
-        for (c = 0; c < 8; c++) {
+        for (c = 0; c < 7; c++) {
             updateGridItem(r, c);
         }
     }
