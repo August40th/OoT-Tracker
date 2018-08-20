3bc5f72130566e32a6f77c5611c5906ac3eaf4d0
August40th <40985705+August40th@users.noreply.github.com>
2018-08-20 21:26:37 +0000

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 var medallions = [];
-var dungeonImg = ['Unknown', '', 'Gohma', 'Dodongo', 'Jellyfish', 'Bow0', 'FireDancer', 'Clam', 'IronKnuckle', 'HoverBoots']
+var dungeonImg = ['Unknown', '', 'Gohma', 'Dodongo', 'Jellyfish', 'Bow0', 'FireDancer', 'Clam', 'IronKnuckle', 'Gibdo']
 ganonlogic = 'Open';
 showprizes = false;
 
