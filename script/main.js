8728f07681156f9421ff16130883a193127d1ed5
August40th <40985705+August40th@users.noreply.github.com>
2019-01-01 12:42:52 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -5,7 +5,7 @@ var fortresskeys = [];
 var keyimg = ['Membership1', 'Membership2', 'Membership3', 'Membership4'];
 
 var songs = [];
-var songimg = ['Unknown', 'NocturneofShadow', 'SongofTime', 'SerenadeofWater', 'RequiemofSpirit' , 'BoleroofFire' , 'MinuetofForest' , 'PreludeofLight', 'SunsSong', 'SongofStorms', 'SariasSong', 'EponasSong', 'ZeldasLullaby'];
+var songimg = ['Unknown', 'NocturneofShadow', 'Ocarina2', 'SerenadeofWater', 'RequiemofSpirit' , 'BoleroofFire' , 'MinuetofForest' , 'PreludeofLight', 'SunsSong', 'SongofStorms', 'SariasSong', 'EponasSong', 'ZeldasLullaby'];
 
 ganonlogic = 'Open';
 gerudobridge ='Default';
