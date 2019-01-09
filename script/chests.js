ca50f11cebb46999a87b51932626383301578824
August40th <40985705+August40th@users.noreply.github.com>
2019-01-09 18:25:22 -0500

    Update chests.js

--- script/chests.js
+++ script/chests.js
@@ -907,7 +907,7 @@ var chests = [
         x: "36.1%",
         y: "39.0%",
         isAvailable: function(){
-            if (items.Magic && ((items.Dins && (items.Bombs || items.Bombchu) && items.Boomerang) || {((items.Bow || items.Fire) || items.Dins) && items.Hokkshot && (items.Bombs || items.Hammer || items.Bombchu)))
+            if (items.Magic && ((items.Dins && (items.Bombs || items.Bombchu) && items.Boomerang) || {((items.Bow || items.Fire) || items.Dins) && items.Hokkshot && (items.Bombs || items.Hammer || items.Bombchu))))
                 return "available";
             return "unavailable";
         }
