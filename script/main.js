f3b94b8f4e35d496acb67cc61826c943430b0d9a
August40th <40985705+August40th@users.noreply.github.com>
2019-02-13 14:32:53 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -928,11 +928,11 @@ function gridItemClick(row, col, corner) {
             if (corner == 3) {
                 fortresskeys[item]++;
                  if (Rescue1){
-                    if (fortresskeys[item] >=  5){
+                    if (fortresskeys[item] >=  2){
                         fortresskeys[item] = 0;}
                 }
                 if (!Rescue1) {
-                    if (fortresskeys[item] >=  2){
+                    if (fortresskeys[item] >=  5){
                         fortresskeys[item] = 0;}
                 }
             }
