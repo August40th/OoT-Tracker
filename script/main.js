0df66ff2a21c069dd9d296ecd5037d206e630a6f
August40th <40985705+August40th@users.noreply.github.com>
2019-03-04 12:29:55 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -391,7 +391,6 @@ function setFortressLogic(sender) {
 
 function setCarpenter(sender) {
     Rescue1 = sender.checked;
-    keyimg = 0;
     if (Rescue1 && keyimg >= 2) {
         keyimg = 0;}
     updateGridItemAll();
