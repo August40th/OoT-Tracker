b3c5ed75d6d2570b6577f07e2e42bcbd06975adf
August40th <40985705+August40th@users.noreply.github.com>
2020-04-15 15:22:56 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -577,7 +577,7 @@ function drawDungeonList() {
          if (document.querySelectorAll("#submaplist .d-none").length) {
             if (submaparea.getAttribute("data-filter") === "vanilla") {
                submaparea.setAttribute("data-filter", "master");
-               submaparea.name = submaparea.textContent + " MQ ";
+               submaparea.name = submaparea.innerHTML + " MQ ";
                
                submaplist.childNodes.forEach(function(item, index) {
                   let itemType = item.getAttribute("data-type");
