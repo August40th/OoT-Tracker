4381f557573957fbc8f8b88291d52c2051eb61f6
August40th <40985705+August40th@users.noreply.github.com>
2020-04-15 15:11:14 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -577,7 +577,8 @@ function drawDungeonList() {
          if (document.querySelectorAll("#submaplist .d-none").length) {
             if (submaparea.getAttribute("data-filter") === "vanilla") {
                submaparea.setAttribute("data-filter", "master");
-                              
+               submaparea.name = submaparea.name + " MQ ";
+               
                submaplist.childNodes.forEach(function(item, index) {
                   let itemType = item.getAttribute("data-type");
                   if (itemType && itemType.startsWith("MQ")) {
