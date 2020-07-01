31a8a42b85bf0b5414b1cf64829e0f3f49e72601
August40th <40985705+August40th@users.noreply.github.com>
2020-06-30 22:12:51 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1142,7 +1142,8 @@ function setBean(sender) {
    BeanShuffle = sender.checked;
    if (BeanShuffle == true)
       dungeons[25].chestlist['Bean Salesman'].isOpened = true;
-   else dungeons[25].chestlist['Bean Salesman'].isOpened = false;
+   else (BeanShuffle == false)
+      dungeons[25].chestlist['Bean Salesman'].isOpened = false;
    updateMap();
    drawDungeonList();
    saveCookie();
