af65650e421baf9634f6d789024c2f99ca68e28a
August40th <40985705+August40th@users.noreply.github.com>
2020-06-30 21:30:04 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1675,7 +1675,7 @@ function updateMap() {
                   DCcount--;
                if (WeirdEgg == false && key == "Malons Weird Egg")
                   DCcount--;
-               if (BeanShuffle == false && key == "Bean Salesman" && dungeons[k].chestlist[key].className == "DCavailable")
+               if (BeanShuffle == false && key == "Bean Salesman")
                   DCcount--;
                if (!dungeons[k].chestlist[key].isOpened && dungeons[k].chestlist[key].isAvailable())
                   DCcount++;
@@ -1720,8 +1720,8 @@ function updateMap() {
                      DCcount--;
                   if (WeirdEgg == false && key == "Malons Weird Egg")
                      DCcount--;
-                  if (BeanShuffle == false && key == "Bean Salesman" && dungeons[k].chestlist[key].className == "DCavailable")
-                  DCcount--;
+                  if (BeanShuffle == false && key == "Bean Salesman")
+                     DCcount--;
                   if (!dungeons[k].chestlist[key].isOpened && dungeons[k].chestlist[key].isAvailable())
                      DCcount++;
                }
@@ -1916,7 +1916,7 @@ function populateMapdiv() {
                   DCcount--;
                if (WeirdEgg == false && key == "Malons Weird Egg")
                   DCcount--;
-               if (BeanShuffle == false && key == "Bean Salesman" && dungeons[k].chestlist[key].className == "DCavailable")
+               if (BeanShuffle == false && key == "Bean Salesman")
                   DCcount--;
                if (!dungeons[k].chestlist[key].isOpened && dungeons[k].chestlist[key].isAvailable())
                   DCcount++;
@@ -1961,8 +1961,8 @@ function populateMapdiv() {
                      DCcount--;
                   if (WeirdEgg == false && key == "Malons Weird Egg")
                      DCcount--;
-                  if (BeanShuffle == false && key == "Bean Salesman" && dungeons[k].chestlist[key].className == "DCavailable")
-                  DCcount--;
+                  if (BeanShuffle == false && key == "Bean Salesman")
+                     DCcount--;
                   if (!dungeons[k].chestlist[key].isOpened && dungeons[k].chestlist[key].isAvailable())
                      DCcount++;
                }
