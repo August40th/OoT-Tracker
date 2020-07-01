062f163c840d004940dfd7b68c736ef9559e5be8
August40th <40985705+August40th@users.noreply.github.com>
2020-06-30 20:53:50 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1675,8 +1675,8 @@ function updateMap() {
                   DCcount--;
                if (WeirdEgg == false && key == "Malons Weird Egg")
                   DCcount--;
-               if (BeanShuffle == false && key == "Bean Salesman" && key.className == "DCunavailable")
-                  DCcount--;
+               if (BeanShuffle == false && key == "Bean Salesman")
+                  dungeons[k].chestlist[key].className = "DCopened";
                if (!dungeons[k].chestlist[key].isOpened && dungeons[k].chestlist[key].isAvailable())
                   DCcount++;
             }
@@ -1720,8 +1720,8 @@ function updateMap() {
                      DCcount--;
                   if (WeirdEgg == false && key == "Malons Weird Egg")
                      DCcount--;
-               if (BeanShuffle == false && key == "Bean Salesman" && key.className == "DCunavailable")
-                  DCcount--;
+                  if (BeanShuffle == false && key == "Bean Salesman")
+                     dungeons[k].chestlist[key].className = "DCopened";
                   if (!dungeons[k].chestlist[key].isOpened && dungeons[k].chestlist[key].isAvailable())
                      DCcount++;
                }
@@ -1916,8 +1916,8 @@ function populateMapdiv() {
                   DCcount--;
                if (WeirdEgg == false && key == "Malons Weird Egg")
                   DCcount--;
-               if (BeanShuffle == false && key == "Bean Salesman" && key.className == "DCunavailable")
-                  DCcount--;
+               if (BeanShuffle == false && key == "Bean Salesman")
+                  dungeons[k].chestlist[key].className = "DCopened";
                if (!dungeons[k].chestlist[key].isOpened && dungeons[k].chestlist[key].isAvailable())
                   DCcount++;
             }
