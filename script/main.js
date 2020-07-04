a80887944ce80172640933f1e3664ec4f51a495f
August40th <40985705+August40th@users.noreply.github.com>
2020-07-04 12:10:28 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2079,7 +2079,6 @@ function updateMap() {
          }
          child = child.nextSibling;
       }
-      updateSkullIcon();
 
    document.getElementById('submaparea').className = "DC" + getDungeonAvailability(dungeons[dungeonSelect]);
    var itemlist = document.getElementById('submaplist').children
@@ -2100,7 +2099,9 @@ function updateMap() {
             itemlist[item].classList.add("d-none");
       } 
    }
-;}
+;
+}
+;
 }
 
 function itemConfigClick(sender) {
