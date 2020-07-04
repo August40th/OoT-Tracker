c52b79a4f09b36b13e584ceea144ddaed82fe3e9
August40th <40985705+August40th@users.noreply.github.com>
2020-07-03 18:11:54 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -517,13 +517,13 @@ function drawDungeonList() {
                      shopnum = 1;
                }
                else {
-                  if (shopsize == 3 && shopitem > 0) 
+                  if (shopsize == 3 && shopitem <= 0) 
                   {}
                   else if (shopsize == 2 && shopitem <= 0)
                   {}
                   else if (shopsize == 1 && shopitem <= 0)
                   {}
-                  else if (shopitem == 0 && shopnum < 0) shopitem = shopsize;
+                  else if (shopitem == 0 && shopnum > 1) shopitem = shopsize;
                }
                started = false;
 
