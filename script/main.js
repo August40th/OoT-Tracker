c5d0c56a95f51a2b90c2cc1a6dfd238b5537e0c9
August40th <40985705+August40th@users.noreply.github.com>
2020-07-13 13:35:38 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1474,7 +1474,7 @@ function setGrottoER(sender) {
 
 function setIndoorER(sender) {
    IndoorER = sender.checked;
-   if (IndoorER = false) {
+   if (IndoorER == false) {
       document.getElementById('selectedindoors').style.display = none;
       document.getElementById('indoorlist').style.display = none;
    }
@@ -1490,7 +1490,7 @@ function setIndoorER(sender) {
 
 function setOWER(sender) {
    OWERmap = sender.checked;
-   if (OWERmap = false)
+   if (OWERmap == false)
       document.getElementById('ermapdiv').style.display = none;
    else document.getElementById('ermapdiv').style.display = "";
    drawERList();
