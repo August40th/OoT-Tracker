8a63021f153d91f0a695dc92329e796b077dcf81
August40th <40985705+August40th@users.noreply.github.com>
2020-07-25 21:01:11 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2632,6 +2632,12 @@ function getDungeonAvailability(dungeon) {
                   {}
                   else if (GrottoER == false && (key.includes("Octorok Grotto") || key.includes("Fountain Grotto") ))
                   {}
+                  else if (shopsize == 1 && key.includes("Shop ") )
+                  {}
+                  else if (shopsize == 2 && (key.includes("Shop 3") || key.includes("Shop 4") ) )
+                  {}
+                  else if (shopsize == 3 && key.includes("Shop 4") )
+                  {}
                   else if (!list[key].isOpened && list[key].isAvailable()) {
                       canGet++;
                   }
@@ -2732,6 +2738,12 @@ function getDungeonAvailability(dungeon) {
                else if (Cowsanity == false && GrottoER == false && key == "Cow Milk Grotto" ) 
                {}
                else if (GrottoER == false && (key.includes("Octorok Grotto") || key.includes("Fountain Grotto") ))
+               {}
+               else if (shopsize == 1 && key.includes("Shop ") )
+               {}
+               else if (shopsize == 2 && (key.includes("Shop 3") || key.includes("Shop 4") ) )
+               {}
+               else if (shopsize == 3 && key.includes("Shop 4") )
                {}
                else if (!list[key].isOpened && list[key].isAvailable()) {
                    canGet++;
