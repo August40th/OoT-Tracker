058cd09d3e84f774b618695265eca6094011254a
August40th <40985705+August40th@users.noreply.github.com>
2021-06-04 00:14:38 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1679,10 +1679,10 @@ function populateMapdiv() {
                 continue;}
             if ( dungeons[k] == dungeons[31] && dungeons[k].chestlist[key].type == "carpet" && Aladdin == false ) { //Carpet Sale
                 continue;}
-            if ( dungeons[dungeonSelect].chestlist[key].type == "ocarina" && OcarinaShuffle == false && 
-            (dungeons[dungeonSelect] == dungeons[32] ||
-            (dungeons[dungeonSelect] == dungeons[18] && OWERmap == false) ||
-            (dungeons[dungeonSelect] == dungeons[0] && OWERmap == true)
+            if ( dungeons[k].chestlist[key].type == "ocarina" && OcarinaShuffle == false && 
+            (dungeons[k] == dungeons[32] ||
+            (dungeons[k] == dungeons[18] && OWERmap == false) ||
+            (dungeons[k] == dungeons[0] && OWERmap == true)
             )
             ) { //Ocarina shuffle for field
                 continue;}
