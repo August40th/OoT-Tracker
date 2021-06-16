5cb72c0283f03c9e94ad0ce7f8f88494124af3d7
August40th <40985705+August40th@users.noreply.github.com>
2021-06-15 22:59:43 -0600

    Update chests.js

--- script/chests.js
+++ script/chests.js
@@ -5186,8 +5186,7 @@ var dungeons = [
                      (
                         (Age == "Child" || OpenDoor == true || (items.Ocarina && items.SongofTime) ) //Can get child
                         && (OpenFountain || items.ZoraLetter) && (items.Bombs || (items.Bombchu && BombchuLogic) || items.Scale) //Can get into domain
-                     )
-                   );
+               ) ;
             }
          },
          ['Iceberg Freestanding']: {
