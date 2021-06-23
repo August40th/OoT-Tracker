ed2dae1969a5b5a15c194d868b7abb2f770075ce
August40th <40985705+August40th@users.noreply.github.com>
2021-06-22 23:46:42 -0600

    Update chests.js

--- script/chests.js
+++ script/chests.js
@@ -54,8 +54,8 @@ function generalCanGetChest(chestlist) {
             continue;}
         if (chestlist[key].type == "gossip" && items.StoneofAgony == false) { //Gossip stones
             continue;}
-        if (chestlist[key].access == "vanilla" && Outer.chestlist.mixedtype == "master" && quest == "Mixed") {
-            continue;}
+        if (chestlist in dungeons[this])
+        { print(this);}
       if (chestlist.hasOwnProperty(key)) {
          if (!chestlist[key].isOpened)
             unopened++;
