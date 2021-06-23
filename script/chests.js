7b8c7833b700564c0bb0478624ad87e86a6aa0b0
August40th <40985705+August40th@users.noreply.github.com>
2021-06-22 22:30:37 -0600

    Update chests.js

--- script/chests.js
+++ script/chests.js
@@ -54,9 +54,9 @@ function generalCanGetChest(chestlist) {
             continue;}
         if (chestlist[key].type == "gossip" && items.StoneofAgony == false) { //Gossip stones
             continue;}
-        if (chestlist[key].access == "vanilla" && Parent.chestlist.mixedtype == "master") {
+        if (chestlist[key].access == "vanilla" && Parent.mixedtype == "master") {
             continue;}
-        if (chestlist[key].access == "master" && Parent.chestlist.mixedtype == "vanilla") {
+        if (chestlist[key].access == "master" && Parent.mixedtype == "vanilla") {
             continue;}
       }
       if (chestlist.hasOwnProperty(key)) {
