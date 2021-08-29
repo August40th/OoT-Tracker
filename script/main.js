4fad4c8575a7fe29c1987fe707201249e7be5428
August40th <40985705+August40th@users.noreply.github.com>
2021-08-29 16:31:44 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -699,6 +699,7 @@ function bulkDCSelect() {
    document.querySelectorAll(selector).forEach
    (function (dungeon) {
        for (let key in dungeons[dungeonSelect].chestlist) {
+           dungeon.innerHTML = key;
            if (dungeons[dungeonSelect].chestlist[dungeon.innerHTML].isOpened !== undefined ) {
                dungeons[dungeonSelect].chestlist[dungeon.innerHTML].isOpened = opening;
            }
