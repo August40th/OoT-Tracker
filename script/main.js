cd8f3d94f4afe5da0e3dd9bd2a47c64b799dba50
August40th <40985705+August40th@users.noreply.github.com>
2021-09-13 14:27:19 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1974,6 +1974,34 @@ function updateMap() {
                     DCcount++;
                 }
                 var minimap = document.getElementById('minimapdiv');
+                if ( (dungeons[dungeonSelect].chestlist[key].floor == here && dungeons[dungeonSelect].type == "dungeon") || dungeons[dungeonSelect].type == "overworld") {
+                    c = document.createElement('span');
+                    c.innerHTML = 'x';
+                    c.id = dungeons[dungeonSelect].chestlist[key].type;
+                    c.className = key + ' ' + c.className;
+                    c.style.cursor = 'pointer';
+                    c.style.position = 'absolute';
+                    c.style.width = '16px';
+                    c.onclick = new Function('toggleDungeonChest(this,' + dungeonSelect + ',"' + key + '")');
+                    c.style.top = dungeons[dungeonSelect].chestlist[key].y;
+                    c.style.left = dungeons[dungeonSelect].chestlist[key].x;
+                    minimap.appendChild(c);
+
+                    var cc = document.createElement('span');
+                    cc.className = 'minimap ' + key;
+                    if (dungeons[dungeonSelect].chestlist[key].leadsto == "unknown") cc.innerHTML = key;
+                    else if (dungeons[dungeonSelect].chestlist[key].leadsto !== "unknown") cc.innerHTML = dungeons[dungeonSelect].chestlist[key].leadsto;
+                    cc.id = 'minimap ' + key;
+                    cc.style.width = '160px';
+                    cc.style.backgroundColor = 'black';
+                    cc.style.color = '#fff';
+                    cc.style.position = 'absolute';
+                    cc.style.textAlign = 'center';
+                    cc.style.fontSize = '20px';
+                    c.appendChild(cc);
+                    c.onmouseover = new Function('highlight(this' + ',"' + key + '")');
+                    c.onmouseout = new Function('unhighlight(this' + ',"' + key + '")');
+                }
                 var check = document.getElementById(key);
                 for (key in dungeons[dungeonSelect].chestlist) {
                     if (check == null) {
@@ -1986,34 +2014,6 @@ function updateMap() {
                     }
                     else {
                         check.className = 'DCunavailable';
-                    }
-                    if ( (dungeons[dungeonSelect].chestlist[key].floor == here && dungeons[dungeonSelect].type == "dungeon") || dungeons[dungeonSelect].type == "overworld") {
-                        c = document.createElement('span');
-                        c.innerHTML = 'x';
-                        c.id = dungeons[dungeonSelect].chestlist[key].type;
-                        c.className = key + ' ' + s.className;
-                        c.style.cursor = 'pointer';
-                        c.style.position = 'absolute';
-                        c.style.width = '16px';
-                        c.onclick = new Function('toggleDungeonChest(this,' + dungeonSelect + ',"' + key + '")');
-                        c.style.top = dungeons[dungeonSelect].chestlist[key].y;
-                        c.style.left = dungeons[dungeonSelect].chestlist[key].x;
-                        minimap.appendChild(c);
-
-                        var cc = document.createElement('span');
-                        cc.className = 'minimap ' + key;
-                        if (dungeons[dungeonSelect].chestlist[key].leadsto == "unknown") cc.innerHTML = key;
-                        else if (dungeons[dungeonSelect].chestlist[key].leadsto !== "unknown") cc.innerHTML = dungeons[dungeonSelect].chestlist[key].leadsto;
-                        cc.id = 'minimap ' + key;
-                        cc.style.width = '160px';
-                        cc.style.backgroundColor = 'black';
-                        cc.style.color = '#fff';
-                        cc.style.position = 'absolute';
-                        cc.style.textAlign = 'center';
-                        cc.style.fontSize = '20px';
-                        c.appendChild(cc);
-                        c.onmouseover = new Function('highlight(this' + ',"' + key + '")');
-                        c.onmouseout = new Function('unhighlight(this' + ',"' + key + '")');
                     }
                 }
             }
