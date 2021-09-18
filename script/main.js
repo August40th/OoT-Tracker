c20f411c851cfbf8e6b23f9e51b2ea80c24aa7f4
August40th <40985705+August40th@users.noreply.github.com>
2021-09-18 17:51:44 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1073,10 +1073,10 @@ function findAngle(x1, y1, x2, y2) {
     if (y1 < y2) distanceY = y2 - y1;
     else if (y1 > y2) distanceY = y1 - y2;
     angle = Math.atan2(distanceY, distanceX);
-    if (y2 < y1 && x2 < x1) angle = Math.atan2(distanceY, -distanceX); //top l
-    else if (y2 > y1 && x2 < x1) angle = Math.atan2(-distanceY, -distanceX); //bottom l
-    else if (y2 < y1 && x2 > x1) angle = Math.atan2(distanceY, distanceX); //top r
-    else if (y2 > y1 && x2 > x1) angle = Math.atan2(-distanceY, distanceX); //bottom r 
+    if (y2 < y1 && x2 < x1) angle = Math.atan(distanceY/-distanceX); //top l
+    else if (y2 > y1 && x2 < x1) angle = Math.atan(-distanceY/ -distanceX); //bottom l
+    else if (y2 < y1 && x2 > x1) angle = Math.atan(distanceY/ distanceX); //top r
+    else if (y2 > y1 && x2 > x1) angle = Math.atan(-distanceY/ distanceX); //bottom r 
     angle = angle * 180 * Math.PI;
     //do { if (angle >= 360) angle = angle - 360; } while (angle >= 360);
     //do { if (angle < 0) angle = angle + 360; } while (angle < 0);
