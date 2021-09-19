86f7220dbc2b40168b16580e333bb05a9945a425
August40th <40985705+August40th@users.noreply.github.com>
2021-09-19 12:41:04 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1065,7 +1065,7 @@ function toggleDungeonChest(sender, d, c) {
                 e.style.cursor = "pointer";
                 e.onclick = function(setLeadsto){
                                 ent = this.innerHTML;
-                                dungeons[d].chestlist[c].leadsto = dungeons[v].name;
+                                dungeons[d].chestlist[c].leadsto = dungeons[k].name;
                                 clickDungeon(d); }
                 l.appendChild(e);
             }
@@ -1088,7 +1088,7 @@ function toggleDungeonChest(sender, d, c) {
                 e.style.cursor = "pointer";
                 e.onclick = function(setLeadsto){
                                 ent = this.innerHTML;
-                                dungeons[d].chestlist[c].leadsto = dungeons[v].name;
+                                dungeons[d].chestlist[c].leadsto = dungeons[k].name;
                                 clickDungeon(d); }
                 l.appendChild(e);
             }
@@ -1111,7 +1111,7 @@ function toggleDungeonChest(sender, d, c) {
                 e.style.cursor = "pointer";
                 e.onclick = function(setLeadsto){
                                 ent = this.innerHTML;
-                                dungeons[d].chestlist[c].leadsto = dungeons[v].name;
+                                dungeons[d].chestlist[c].leadsto = dungeons[k].name;
                                 clickDungeon(d); }
                 l.appendChild(e);
             }
