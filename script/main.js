b1e738fbfcd8748609d039798f3e9cef758044a0
August40th <40985705+August40th@users.noreply.github.com>
2021-09-28 13:57:33 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -752,7 +752,9 @@ function clickDungeon(d) {
         s.onmouseout = new Function('unhighlightDungeonChest(this)');
         s.style.cursor = "pointer";
 
-        if (dungeons[dungeonSelect].chestlist[key].leadsto !== "unknown" && dungeons[dungeonSelect].chestlist[key].type !== "entrance") DClist.appendChild(s);
+        if (dungeons[dungeonSelect].chestlist[key].leadsto !== "unknown" && dungeons[dungeonSelect].chestlist[key].type == "entrance") {
+        }
+        else { DClist.appendChild(s);}
         
         if ( (dungeons[dungeonSelect].chestlist[key].floor == here && dungeons[dungeonSelect].type == "dungeon" && (dungeons[dungeonSelect].mixedtype == dungeons[dungeonSelect].chestlist[key].access || quest == 'Mixecd') ) || dungeons[dungeonSelect].type == "overworld") {
             var c = document.createElement('span');
