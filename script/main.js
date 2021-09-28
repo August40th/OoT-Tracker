7f0ed90acf8228b955c83c715a2b53e0294c8022
August40th <40985705+August40th@users.noreply.github.com>
2021-09-28 08:31:04 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -627,9 +627,38 @@ function clickDungeon(d) {
                         //ll.style.fontSize = '20';
                         //l.onmouseout = new Function('lineunhighlight(' + ll + ')');
                         //l.onmouseover = new Function('linehighlight(' + ll + ')');
-
                         //l.appendChild(ll);
+                        
                         document.getElementById('mapdiv').appendChild(l);
+                    }
+                    if (dungeons[v].chestlist[ent].leadsto !== "unknown" && ( (OWERmap == true && dungeons[v].chestlist[ent].type == "entrance") || 
+                    (DungeonER == true && dungeons[v].chestlist[ent].type == "dungeon") || 
+                    (Owls == true && dungeons[v].chestlist[ent].type == "owl") ||
+                    (Warps == true && dungeons[v].chestlist[ent].type == "warp") ||
+                    (RndmStart == true && dungeons[v].chestlist[ent].type == "spawn") ) ) {
+                        for ( var u = 0; u < dungeons.length; u++) {
+                            if (u == dungeonSelect || u == v) continue;
+                            for (var nxt in dungeons[u].chestlist) {
+                                if (dungeons[v].chestlist[ent].leadsto == nxt || dungeons[v].chestlist[ent].leadsto == dungeons[u].name + ' ' + nxt || dungeons[v].chestlist[ent].leadsto == dungeons[u].name) {
+                                    let x1 = parseFloat(dungeons[v].x)*mapDivWidth/100;
+                                    let y1 = parseFloat(dungeons[v].y)*mapDivHeight/100;
+                                    let x2 = parseFloat(dungeons[u].x)*mapDivWidth/100;
+                                    let y2 = parseFloat(dungeons[u].y)*mapDivHeight/100;
+
+                                    var j = document.createElement('eline');
+                                    j.id = 'eline';
+                                    j.style.height = '4px';
+                                    j.style.width = findDistance(x1, y1, x2, y2 ) + 'px';
+                                    j.style.transformOrigin = "0 0";
+                                    j.style.transform = 'rotate('+ findAngle(x1, y1, x2, y2 ) + 'deg)';
+                                    j.style.position = 'absolute';
+                                    j.style.whiteSpace = 'nowrap';
+                                    j.style.backgroundColor = 'yellow';
+                                    j.style.left = x1+"px";
+                                    j.style.top = y1+"px";
+                                    document.getElementById('mapdiv').appendChild(j); }
+                            }
+                        }
                     }
                 } }
         }
