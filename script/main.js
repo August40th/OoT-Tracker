cb31455e0dcb3cdff2c59c3f5bbb8b4e66d7e62f
August40th <40985705+August40th@users.noreply.github.com>
2021-09-29 18:03:37 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -610,7 +610,7 @@ function clickDungeon(d) {
             for ( var v = 0; v < dungeons.length; v++) {
                 if (v == dungeonSelect) continue;
                 for (var ent in dungeons[v].chestlist) {
-                    if ( (dungeons[v].chestlist[ent].type == "entrance" || dungeons[v].chestlist[ent].type == "dungeon" || (mixphoo !== "Off" && (dungeons[dungeonSelect].chestlist[key].type == "simple" || dungeons[dungeonSelect].chestlist[key].type == "alldoor" || dungeons[dungeonSelect].chestlist[key].type == "grotto") )) && (dungeons[dungeonSelect].chestlist[key].leadsto == ent || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name + ' ' + ent || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name + ' Dungeon Door' || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name)) {
+                    if (dungeons[dungeonSelect].chestlist[key].leadsto == ent || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name + ' ' + ent || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name + ' Dungeon Door' || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name) {
                         let mapDivWidth = 828;
                         let mapDivHeight = 420;
 
@@ -661,10 +661,7 @@ function clickDungeon(d) {
                         for ( var u = 0; u < dungeons.length; u++) {
                             if (u == dungeonSelect || u == v) continue;
                             for (var nxt in dungeons[u].chestlist) {
-                                if ( (dungeons[v].chestlist[ent].type == "entrance" || dungeons[v].chestlist[ent].type == "dungeon" || (mixphoo !== "Off" && (dungeons[dungeonSelect].chestlist[key].type == "simple" || dungeons[dungeonSelect].chestlist[key].type == "alldoor" || dungeons[dungeonSelect].chestlist[key].type == "grotto") ) ) &&
-                                    (dungeons[u].chestlist[nxt].type == "entrance" || dungeons[u].chestlist[nxt].type == "dungeon" || (mixphoo !== "Off" && (dungeons[dungeonSelect].chestlist[key].type == "simple" || dungeons[dungeonSelect].chestlist[key].type == "alldoor" || dungeons[dungeonSelect].chestlist[key].type == "grotto") )) &&
-                                    (dungeons[v].chestlist[ent].leadsto == nxt || dungeons[v].chestlist[ent].leadsto == dungeons[u].name + ' ' + nxt || 
-                                     dungeons[v].chestlist[ent].leadsto == dungeons[u].name) ) {
+                                if (dungeons[v].chestlist[ent].leadsto == nxt || dungeons[v].chestlist[ent].leadsto == dungeons[u].name + ' ' + nxt || dungeons[v].chestlist[ent].leadsto == dungeons[u].name + ' Dungeon Door' || dungeons[v].chestlist[ent].leadsto == dungeons[u].name) {
                                     for (var o in dungeons[v].chestlist) {
                                         if (dungeons[dungeonSelect].chestlist[key].leadsto == o || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name + ' ' + o || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name + ' Dungeon Door' || dungeons[dungeonSelect].chestlist[key].leadsto == ' Dungeon Door' || dungeons[dungeonSelect].chestlist[key].leadsto == dungeons[v].name) {
                                             let mapDivWidth = 828;
@@ -699,8 +696,8 @@ function clickDungeon(d) {
                                             jj.style.fontSize = '24px';
                                             jj.style.visibility = 'hidden';
                                             j.appendChild(jj);
-                                            j.onmouseover = new Function('highlight(this' + ',"' + nxt + '")');
-                                            j.onmouseout = new Function('unhighlight(this' + ',"' + nxt + '")'); }}}}}
+                                            j.onmouseover = new Function('linehighlight(this' + ',"' + nxt + '")');
+                                            j.onmouseout = new Function('lineunhighlight(this' + ',"' + nxt + '")'); }}}}}
                     }
                 } }
         }
